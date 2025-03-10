8b7a6c23b8396a99105c7cfc5a69e32374b95243
senko496 <szymis02@gmail.com>
2025-03-10 18:43:46 +0100

    commit number 2

--- APBD-merge-types/Program.cs
+++ APBD-merge-types/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program {
+    static void Main() {
+        int[] numbs = {5, 4, 7, 9};
+        Console.WriteLine(CalculateAvg(numbs));
+        Console.WriteLine(CalculateMax(numbs));
+    }
+
+    static double CalculateAvg(int[] numbs) {
+        double sum = 0;
+        foreach (int numb in numbs) {
+            sum += numb;
+        }
+        return sum / numbs.Length; 
+    }
+        static double CalculateMax(int[] numbs) {
+        double max = 0;
+        foreach (int numb in numbs) {
+            if(numb > max) {
+                max = numb; 
+            }
+        }
+        return max;
+    }
+}
