2a2de2758625a95d7cd06a8b130abebb92877a69
senko496 <szymis02@gmail.com>
2025-03-10 19:30:52 +0100

    commit merge squash test 2

--- APBD-merge-types/Program.cs
+++ APBD-merge-types/Program.cs
@@ -5,6 +5,17 @@ class Program {
         int[] numbs = {5, 4, 7, 9};
         Console.WriteLine(CalculateAvg(numbs));
         Console.WriteLine(CalculateMax(numbs));
+
+
+
+
+
+
+
+
+
+
+        Console.WriteLine("HEJ HEJ")
     }
 
     static double CalculateAvg(int[] numbs) {
